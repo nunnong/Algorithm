b3df826a43c61706864123d08053ebc5ee8c0aca
nunnong <juyy99@gmail.com>
2025-01-19 17:55:22 +0900

    [Bronze IV] Title: No Brainer, Time: 152 ms, Memory: 17160 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0 ; i < n ; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            if (x < y) {
+                System.out.println("NO BRAINS");
+            } else {
+                System.out.println("MMM BRAINS");
+            }
+        }
+
+
+    }
+}
