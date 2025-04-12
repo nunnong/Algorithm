2b65c81dfc2cfb7565faca37fa661706b8e58be6
nunnong <juyy99@gmail.com>
2025-04-12 14:35:45 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 552 ms, Memory: 49944 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -7,78 +7,70 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n, m;
-	static int[][] map;
-	static boolean[][] visited;
-	static int startX, startY;
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visited;
+    static int startX, startY;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-		map = new int[n][m];
-		visited = new boolean[n][m];
+        map = new int[n][m];
+        visited = new boolean[n][m];
 
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
 
-				if (map[i][j] == 0) {
-					visited[i][j] = true;
-				}
-				if (map[i][j] == 2) {
-					visited[i][j] = true;
-					startX = i;
-					startY = j;
-				}
-			}
-		}
+                if (map[i][j] == 0) {
+                    visited[i][j] = true;
+                } else if (map[i][j] == 2) {
+                    visited[i][j] = true;
+                    startX = i;
+                    startY = j;
+                }
+            }
+        }
 
-		bfs(startX, startY, 0);
+        bfs(startX, startY);
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j]) map[i][j] = -1;
+                sb.append(map[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
 
-				if (!visited[i][j]) {
-					map[i][j] = -1;
-				}
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
+        System.out.print(sb);
+    }
 
-		}
-	}
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y, 0});
 
-	private static void bfs(int x, int y, int depth) {
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int cx = current[0], cy = current[1], depth = current[2];
+            map[cx][cy] = depth;
 
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] { x, y, depth });
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
-		while (!queue.isEmpty()) {
-			int[] tmp = queue.poll();
-			x = tmp[0];
-			y = tmp[1];
-			depth = tmp[2];
-
-			map[x][y] = depth;
-
-			for (int d = 0; d < 4; d++) {
-				int nx = x + dx[d];
-				int ny = y + dy[d];
-
-				if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] != 0 && !visited[nx][ny]) {
-					visited[nx][ny] = true;
-					queue.add(new int[] { nx, ny, depth + 1 });
-				}
-			}
-
-		}
-
-	}
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] != 0 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.offer(new int[]{nx, ny, depth + 1});
+                }
+            }
+        }
+    }
 }
