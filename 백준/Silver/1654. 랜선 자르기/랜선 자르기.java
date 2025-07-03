5a32f83efd56e437f270a68fe35a7c81ab8d5fc6
nunnong <juyy99@gmail.com>
2025-07-03 09:51:40 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17316 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int K, N;
+    static int[] line;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        line = new int[K];
+
+        long sum = 0;
+        for (int i = 0; i < K; i++) {
+            line[i] = Integer.parseInt(br.readLine());
+            sum += line[i];
+        }
+
+        long maxLength = sum / N;
+        long left = 1;
+        long right = maxLength;
+        long ans = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            long cnt = 0; // 랜선의 개수
+            for (int i = 0; i < K; i++) {
+                cnt += line[i] / mid;
+            }
+
+            if (cnt >= N) {
+                ans = mid;
+                left = mid + 1;
+            } else if (cnt < N) {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
