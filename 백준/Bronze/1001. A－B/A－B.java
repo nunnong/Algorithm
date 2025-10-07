7f87c14b4b13a1600c7e8904bd6f8fb937b5723b
nunnong <juyy99@gmail.com>
2025-10-07 23:21:06 +0900

    [Bronze V] Title: A-B, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String[] input = br.readLine().split(" ");
+    int A = Integer.parseInt(input[0]);
+    int B = Integer.parseInt(input[1]);
+
+    if (A > 0 && B < 10) {
+      System.out.println(A - B);
+    }
+  }
+}
