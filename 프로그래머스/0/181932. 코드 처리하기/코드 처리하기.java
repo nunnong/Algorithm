e17322af0523847771ff0138655c13917c284e51
정윤영 <juyy99@gmail.com>
2025-12-30 00:32:15 +0900

    [level 0] Title: 코드 처리하기, Time: 6.65 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,24 +1,29 @@
 class Solution {
     public String solution(String code) {
-
-        StringBuilder ret = new StringBuilder();
+        
         boolean mode = false;
-
-        for (int i = 0; i < code.length(); i++) {
-            char c = code.charAt(i);
-
-            if (c == '1') {
+        
+        char[] text = code.toCharArray();
+        
+        StringBuilder ret = new StringBuilder();
+        
+        for (int i = 0 ; i < text.length ; i++) {
+            if (text[i] == '1') {
                 mode = !mode;
-                continue; 
+                continue;
             }
-
+            
             if (!mode && i % 2 == 0) {
-                ret.append(c);
-            } else if (mode && i % 2 == 1) {
-                ret.append(c);
+                ret.append(text[i]);
+                continue;
+            }
+            
+            if (mode && i % 2 == 1) {
+                ret.append(text[i]);
+                continue;
             }
         }
-
-        return ret.length() == 0 ? "EMPTY" : ret.toString();
+        
+        return (ret.length() == 0) ? "EMPTY" : ret.toString();
     }
 }
