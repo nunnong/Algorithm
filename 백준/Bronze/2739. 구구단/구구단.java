2dd774ca73ade76261d360a88600407dc3d07b4b
정윤영 <juyy99@gmail.com>
2026-01-15 09:23:43 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i <= 9; i++) {
+      sb.append(N).append(" * ").append(i).append(" = ").append(N * i).append("\n");
+    }
+
+    System.out.println(sb.toString());
+  }
+}
