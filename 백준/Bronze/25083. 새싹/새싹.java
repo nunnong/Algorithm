53567e5b061f3aba60ac0ae12edbcc366dedfd00
정윤영 <juyy99@gmail.com>
2026-01-22 22:52:45 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(
+        "         ,r'\"7\n" +
+        "r`-_   ,'  ,/\n" +
+        " \\. \". L_r'\n" +
+        "   `~\\/\n" +
+        "      |\n" +
+        "      |"
+    );
+
+    System.out.println(sb.toString());
+  }
+}
