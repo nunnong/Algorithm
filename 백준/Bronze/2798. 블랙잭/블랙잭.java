6fa7938467569f2fa9b8fca75acc1104295d9c20
정윤영 <juyy99@gmail.com>
2026-01-28 09:21:40 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,64 +1,41 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, minDiffer;
-	static int[] arr, data;
-	static boolean[] visited;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    int[] arr = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
 
-		arr = new int[N];
-		data = new int[3];
+    int maxSum = 0;
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+    for (int i = 0; i < N; i++) {
+      for (int j = i + 1; j < N; j++) {
+        for (int k = j + 1; k < N; k++) {
+          int sum = arr[i] + arr[j] + arr[k];
 
-		minDiffer = M;
-		combination(0, 0);
+          if (sum == M) {
+            maxSum = M;
+            break;
+          }
 
-		System.out.println(M - minDiffer);
+          if (maxSum < sum && sum < M) {
+            maxSum = sum;
+          }
+        }
+      }
+    }
 
-	}
-
-	private static void combination(int idx, int start) {
-		// TODO Auto-generated method stub
-
-		if (idx == 3) {
-			int sum = 0;
-
-			for (int i : data) {
-				sum += i;
-			}
-			
-			if (sum == M) {
-				minDiffer = 0;
-				return;
-			}
-
-			if (sum <= M && minDiffer > M - sum) {
-				minDiffer = M - sum;
-			}
-
-			return;
-		}
-
-		for (int i = start; i < N; i++) {
-
-			data[idx] = arr[i];
-
-			combination(idx + 1, i + 1);
-		}
-	}
-
+    System.out.println(maxSum);
+  }
 }
