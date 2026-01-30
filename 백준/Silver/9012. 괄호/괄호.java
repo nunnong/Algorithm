58873fe06924c7edafc1ebacce8fe99861800582
정윤영 <juyy99@gmail.com>
2026-01-30 10:37:01 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,33 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());  // 테스트 케이스 개수 입력
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < T; i++) {
-            String input = br.readLine();  // 괄호 문자열 입력
-            System.out.println(isVPS(input) ? "YES" : "NO");
+    int T = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < T; i++) {
+      char[] arr = br.readLine().toCharArray();
+
+      Deque<Character> deque = new ArrayDeque<>();
+      boolean isVPS = true;
+
+      for (int j = 0; j < arr.length; j++) {
+        if (arr[j] == '(') {
+          deque.add('(');
+          continue;
         }
+        if (deque.isEmpty()) {
+          isVPS = false;
+          break;
+        } else {
+          deque.pollLast();
+        }
+      }
+
+      if (!deque.isEmpty()) {
+        isVPS = false;
+      }
+
+      if (isVPS) {
+        sb.append("YES").append("\n");
+      } else {
+        sb.append("NO").append("\n");
+      }
     }
 
-    // 괄호 문자열이 올바른지 판단하는 함수
-    public static boolean isVPS(String s) {
-        Stack<Character> stack = new Stack<>();
+    System.out.println(sb.toString());
 
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                stack.push(ch);  // 여는 괄호는 스택에 넣음
-            } else { // 닫는 괄호인 경우
-                if (stack.isEmpty()) {
-                    return false;  // 스택이 비어있으면 올바른 괄호 문자열이 아님
-                }
-                stack.pop();  // 한 쌍이 맞으므로 스택에서 제거
-            }
-        }
-        return stack.isEmpty();  // 스택이 비어있어야 올바른 VPS
-    }
+  }
 }
