a20ca1f374ddb8b40aa48a55f5dd6cceb1558f80
정윤영 <juyy99@gmail.com>
2026-02-01 11:58:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 636 ms, Memory: 141980 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static ArrayList<Integer>[] graph;
+  static boolean[] visited;
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    graph = new ArrayList[N + 1];
+    visited = new boolean[N + 1]; // 정점을 방문했는지 체크
+
+    for (int i = 0; i <= N; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+
+      int s = Integer.parseInt(st.nextToken());
+      int e = Integer.parseInt(st.nextToken());
+
+      graph[s].add(e);
+      graph[e].add(s); // 정점 기준 연결 완료
+    }
+
+    int cnt = 0; // 연결 요소 개수
+    for (int i = 1; i <= N; i++) {
+      if (visited[i]) {
+        continue;
+      }
+      cnt++;
+      DFS(i);
+    }
+
+    System.out.println(cnt);
+  }
+
+  public static void DFS(int i) {
+    if (visited[i]) {
+      return;
+    }
+    visited[i] = true;
+
+    for (int k : graph[i]) {
+      if (!visited[k]) {
+        DFS(k);
+      }
+    }
+  }
+}
