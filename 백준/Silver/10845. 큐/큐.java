96e984d2796880be3d43cc91079bf617b4f3436a
정윤영 <juyy99@gmail.com>
2026-02-05 14:39:03 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 18944 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,48 +1,55 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int N = Integer.parseInt(br.readLine()); // 명령어 개수
-		Queue<String> queue = new LinkedList<>();
-		
-		for (int i = 0; i < N; i++) {
-            String[] tmp = br.readLine().trim().split(" ");
-            
-            switch (tmp[0]) {
-                case "push":
-                	queue.add(tmp[1]);
-                    break;
-                case "pop":
-                	bw.write((queue.isEmpty() ? -1 : queue.poll()) + "\n");
-                	break;
-                case "size":
-                	bw.write(queue.size() + "\n");
-                	break;
-                case "empty":
-                	bw.write((queue.isEmpty() ? 1 : 0) + "\n");
-                	break;
-                case "front":
-                    bw.write((queue.isEmpty() ? -1 : queue.peek()) + "\n");
-                    break;
-                case "back":
-                    bw.write((queue.isEmpty() ? -1 : ((LinkedList<String>) queue).getLast()) + "\n");
-                    break;
-            }
-        }
-		br.close();
-		bw.flush();
-		bw.close();
-		
-	}// main
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    ArrayDeque<Integer> queue = new ArrayDeque<>();
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      switch (st.nextToken()) {
+        case "push":
+          queue.offer(Integer.parseInt(st.nextToken()));
+          break;
+
+        case "pop":
+          if (queue.isEmpty()) {
+            sb.append(-1).append("\n");
+          } else {
+            sb.append(queue.pop()).append("\n");
+          }
+          break;
+
+        case "size":
+          sb.append(queue.size()).append("\n");
+          break;
+
+        case "empty":
+          sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+          break;
+
+        case "front":
+          sb.append(queue.isEmpty() ? -1 : queue.peekFirst()).append("\n");
+          break;
+
+        case "back":
+          sb.append(queue.isEmpty() ? -1 : queue.peekLast()).append("\n");
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    System.out.println(sb.toString());
+  }
 }
