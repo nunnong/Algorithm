17e228779b1e8cfa699dd1b81e0453b547fef476
정윤영 <juyy99@gmail.com>
2026-02-07 14:23:59 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 352 ms, Memory: 294260 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -7,41 +7,34 @@ import java.util.Queue;
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    Queue<Integer> q = new ArrayDeque<>();
 
     String[] str = br.readLine().split(" ");
 
     int N = Integer.parseInt(str[0]);
     int K = Integer.parseInt(str[1]);
 
-    int[] num = new int[N];
+    Queue<Integer> q = new ArrayDeque<>();
 
     for (int i = 0; i < N; i++) {
-      num[i] = i + 1;
+      q.add(i + 1);
     }
-    boolean[] checked = new boolean[N];
+    StringBuilder sb = new StringBuilder();
+    int k = K;
 
-    int idx = 0;
-    int n = N;
-    while (n-- > 0) {
-      int cnt = 0;
-
-      while (cnt < K) {
-        if (checked[idx]) {
-          idx = (idx + 1) % N;
-          continue;
-        } else {
-          cnt++;
-          if (cnt == K) {
-            checked[idx] = true;
-            q.add(num[idx]);
-          }
-          idx = (idx + 1) % N;
-        }
+    while (!q.isEmpty()) {
+      k = K;
+      while (--k > 0) {
+        int add = q.poll();
+        q.add(add);
       }
+      sb.append(q.poll()).append(',').append(" ");
     }
-    String ans = q.toString().replace('[', '<').replace(']', '>');
-    System.out.println(ans);
+
+    sb.insert(0, "<");
+    sb.setLength(sb.length() - 2);
+    sb.append(">");
+
+    System.out.println(sb.toString());
 
   }
 }
