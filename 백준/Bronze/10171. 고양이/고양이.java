d93dd73d79c8858e7fcaf976dc8d3490a844978d
정윤영 <juyy99@gmail.com>
2026-02-11 22:19:59 +0900

    [Bronze V] Title: 고양이, Time: 84 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,9 +1,12 @@
 public class Main {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
-    }
+    StringBuilder sb = new StringBuilder();
+    sb.append("\\    /\\\n");
+    sb.append(" )  ( ')\n");
+    sb.append("(  /  )\n");
+    sb.append(" \\(__)|");
+
+    System.out.println(sb.toString());
+  }
 }
