beafa52d226802c58a146b724ea64fae53e6b38c
정윤영 <juyy99@gmail.com>
2026-02-22 00:48:49 +0900

    [Silver III] Title: 두 수의 합, Time: 264 ms, Memory: 27816 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,32 +1,47 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    // n * log n 까지 가능 시간초과 -> 더 효율적인 방법! 찾기
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
 
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int result = Integer.parseInt(br.readLine());
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Set<Integer> arr = new HashSet<>();
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
 
-        for (int i = 0 ; i < n ; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (num > result/2) {
-                arr.add(num);
-            } else {
-                arr.add(result - num);
-            }
-        }
-
-        System.out.println(n - arr.size());
-
-
-
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+
+    int x = Integer.parseInt(br.readLine());
+    int ans = 0;
+
+    Arrays.sort(arr);
+    int left = 0;
+    int right = n - 1;
+
+    while (left < right) {
+      if (left == n || right == 0) {
+        break;
+      }
+      int sum = arr[left] + arr[right];
+
+      if (sum == x) {
+        ans++;
+        left++;
+        right--;
+      } else if (sum > x) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+
+    System.out.println(ans);
+
+  }
 }
