ecc90009d8bd725087d7707293fc9f2791a4e73b
정윤영 <juyy99@gmail.com>
2026-02-21 23:52:01 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,19 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int year = scanner.nextInt();
+    int year = Integer.parseInt(br.readLine());
 
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println(1);
-        }else {
-            System.out.println(0);
-        }
-
-
-
-
+    boolean isLeapYear = false;
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+      isLeapYear = true;
     }
+
+    System.out.println(isLeapYear ? 1 : 0);
+  }
+
 }
