3fa78dacdf3636dc34103529a3e819248538d532
정윤영 <juyy99@gmail.com>
2026-02-26 22:34:36 +0900

    [Silver IV] Title: 듣보잡, Time: 276 ms, Memory: 29036 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,34 +1,45 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+    HashMap<String, Integer> names = new HashMap<>();
+    List<String> list = new ArrayList<>();
 
-        HashSet<String> set = new HashSet<>();
-        ArrayList<String> arr = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            set.add(br.readLine());
-        }
-
-        for (int i = 0; i < M; i++) {
-            String name = br.readLine();
-            if (set.contains(name)) {
-                arr.add(name);
-            }
-        }
-
-        System.out.println(arr.size());
-        Collections.sort(arr);
-
-        for (String name : arr) {
-            System.out.println(name);
-        }
+    for (int i = 0; i < N; i++) {
+      names.put(br.readLine(), 1);
     }
+
+    for (int i = 0; i < M; i++) {
+      String name = br.readLine();
+      names.put(name, names.getOrDefault(name, 0) + 1);
+
+      if (names.get(name) == 2) {
+        list.add(name);
+      }
+    }
+
+    Collections.sort(list);
+
+    StringBuilder sb = new StringBuilder();
+    sb.append(list.size());
+
+    for (String str : list) {
+      sb.append("\n");
+      sb.append(str);
+    }
+
+    System.out.println(sb.toString());
+  }
 }
