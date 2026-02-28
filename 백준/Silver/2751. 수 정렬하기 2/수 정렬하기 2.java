0091792e6fc078ba1203e82b16c5405ea39ac075
정윤영 <juyy99@gmail.com>
2026-02-28 23:12:28 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1468 ms, Memory: 113232 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,38 +1,27 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.text.BreakIterator;
 import java.util.Arrays;
 
-public class Main  {
-	// 수 정렬하기2
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		
-		for (int i = 0 ; i < N ; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			
-		}
-		
-		Arrays.sort(arr);
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0 ; i < N ; i++) {
-			sb.append(arr[i]).append("\n");
-		}
-		
-		bw.write(sb.toString());
-		bw.flush();
-		br.close();
-		bw.close();
-		
-	} // main
-	
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    int N = Integer.parseInt(br.readLine());
+
+    int[] arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    StringBuilder sb = new StringBuilder();
+
+    Arrays.sort(arr);
+
+    for (int i : arr) {
+      sb.append(i).append("\n");
+    }
+    System.out.println(sb.toString());
+  }
 }
