ff978f0af21d36e35a6df78a30e55aeadebbd31c
정윤영 <juyy99@gmail.com>
2026-03-10 23:47:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 19776 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,58 +1,53 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken()); // 수빈 위치
-        int K = Integer.parseInt(st.nextToken()); // 동생 위치
+    System.out.println(bfs(N, K));
+  }
 
-        if (N >= K) {
-            System.out.println(N - K);
-            return; // return 추가
+  static int bfs(int start, int target) {
+    if (start == target)
+      return 0;
+
+    boolean[] visited = new boolean[100001];
+    visited[start] = true;
+
+    Queue<Integer> q = new ArrayDeque<>();
+    q.add(start);
+    int cnt = 0;
+
+    while (!q.isEmpty()) {
+      cnt++;
+      int size = q.size();
+
+      while (size-- > 0) {
+        int x = q.poll();
+
+        for (int nx : new int[] { x - 1, x + 1, x * 2 }) {
+          if (nx < 0 || nx >= 100001)
+            continue;
+          if (nx == target)
+            return cnt;
+          if (!visited[nx]) {
+            visited[nx] = true;
+            q.add(nx);
+          }
         }
-
-        boolean[] visited = new boolean[100001];
-        Queue<int[]> queue = new LinkedList<>();
-
-        queue.offer(new int[]{N, 0}); // 수빈 위치, time
-        visited[N] = true; // 시작점 visited 설정
-
-        while (!queue.isEmpty()) {
-
-            int[] node = queue.poll();
-            int p = node[0];
-            int t = node[1];
-
-            if (p == K) {
-                System.out.println(t);
-                return;
-            }
-
-            // p+1로 이동
-            if (p+1 >= 0 && p+1 <= 100000 && !visited[p+1]) {
-                visited[p+1] = true;
-                queue.offer(new int[]{p+1, t+1});
-            }
-
-            // p-1로 이동
-            if (p-1 >= 0 && p-1 <= 100000 && !visited[p-1]) {
-                visited[p-1] = true;
-                queue.offer(new int[]{p-1, t+1});
-            }
-
-            // p*2로 이동
-            if (p*2 >= 0 && p*2 <= 100000 && !visited[p*2]) {
-                visited[p*2] = true;
-                queue.offer(new int[]{p*2, t+1});
-            }
-        }
+      }
     }
+
+    return -1;
+  }
 }
