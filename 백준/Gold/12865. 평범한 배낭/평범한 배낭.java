c25d657a09dc0fbd56a99d1f4ef9766b929efb5c
정윤영 <juyy99@gmail.com>
2026-03-14 16:06:52 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 53784 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,42 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int N, K;
-  static int[] weights;
-  static int[] values;
+  static int N, W;
+  static int[][] dp; // dp[i][j] = i번째 물건까지 봤을 때, 용량j 가방의 최대 가치
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     StringTokenizer st = new StringTokenizer(br.readLine());
+
     N = Integer.parseInt(st.nextToken());
-    K = Integer.parseInt(st.nextToken());
+    W = Integer.parseInt(st.nextToken());
 
-    weights = new int[N];
-    values = new int[N];
+    int[] w = new int[N + 1];
+    int[] v = new int[N + 1];
 
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      weights[i] = Integer.parseInt(st.nextToken());
-      values[i] = Integer.parseInt(st.nextToken());
-    }
-
-    int[][] dp = new int[N + 1][K + 1];
+    dp = new int[N + 1][W + 1];
 
     for (int i = 1; i <= N; i++) {
-      for (int j = 0; j <= K; j++) {
-        dp[i][j] = dp[i-1][j];
+      st = new StringTokenizer(br.readLine());
+      w[i] = Integer.parseInt(st.nextToken());
+      v[i] = Integer.parseInt(st.nextToken());
+    }
 
-        if (j >= weights[i-1]) {
-          dp[i][j] = Math.max(dp[i][j], dp[i-1][j - weights[i-1]] + values[i-1]);
+    for (int i = 1; i <= N; i++) { // 물건을 하나씩 추가
+      for (int j = 0; j <= W; j++) {
+        if (j < w[i]) {
+          dp[i][j] = dp[i - 1][j];
+        } else {
+          dp[i][j] = Math.max(
+              dp[i - 1][j],
+              v[i] + dp[i - 1][j - w[i]]
+          );
         }
       }
     }
 
-    System.out.println(dp[N][K]);
+    System.out.println(dp[N][W]);
   }
 }
