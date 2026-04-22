f531c9ed0e5c63ea90e59db70c3ae511fb7407ee
정윤영 <juyy99@gmail.com>
2026-04-22 22:10:26 +0900

    [level 2] Title: 전화번호 목록, Time: 394.08 ms, Memory: 99.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        int len = phone_book.length;
+        
+        Arrays.sort(phone_book);
+        
+        for (int i = 0 ; i < len - 1 ; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
