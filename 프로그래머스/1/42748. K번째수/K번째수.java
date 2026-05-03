5f382b6205b666c6528f20bd994cda5b6a854ae4
정윤영 <juyy99@gmail.com>
2026-05-03 09:27:18 +0900

    [level 1] Title: K번째수, Time: 1.10 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        
         int[] answer = new int[commands.length];
         
-        // 명령어를 돌아가면서 실행 (명령어 길이만큼)
-        for (int i = 0 ; i < commands.length ; i++) {
+        for (int i = 0; i < commands.length; i++) {
+            int from = commands[i][0] - 1;
+            int to = commands[i][1];
+            int k = commands[i][2] - 1;
             
-            int start = commands[i][0] - 1;
-            int end = commands[i][1] -1;
-            int ans = commands[i][2] -1;
-            
-            int[] arr = Arrays.copyOfRange(array, start, end+1);
-            Arrays.sort(arr);
-            answer[i] = arr[ans];
+            int[] sliced = Arrays.copyOfRange(array, from, to);
+            Arrays.sort(sliced);
+            answer[i] = sliced[k];
         }
         
         return answer;
